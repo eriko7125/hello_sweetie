05b45982277d493a3ac8582c227aa2da0a454521
eriko7125 <t.eriko7125@gmail.com>
2020-02-29 10:18:52 +0000

    Add: address views

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,7 +1,43 @@
 class AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
-end
+
+  def create
+    @address = Address.new(address_params)
+    @address.end_user_id = current_end_user.id
+    if @address.save
+      redirect_to addresses_path, notice: "配送先が登録されました."
+    else
+      @addresses = Address.all
+      render :index
+    end
+  end
+
+    def update
+      @address = Address.find(params[:id])
+      if @address.update(address_params)
+        redirect_to addresses_path, notice: "配送先が編集されました"
+      else
+        render action: :edit
+      end
+    end
+
+    def destroy
+      @address = Address.find(params[:id])
+      @address.destroy
+      redirect_to addresses_path, notice: "配送先が削除されました"
+    end
+
+    private
+    
+    def address_params
+      params.require(:address).permit(:name, :zipcode, :address)
+    end
+    
+  end
