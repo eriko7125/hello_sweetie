3412a16627390364b5761a2b47697164b7ed83e8
eriko7125 <t.eriko7125@gmail.com>
2020-03-12 20:53:27 +0000

    Fix: pagination

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
     # 販売中のアイテム
     @active_items = Item.where(status: 1).page(params[:page]).per(8).order('created_at DESC')
     # 販売停止中のアイテム
-    @items = Item.where(status: 0).page(params[:page]).per(4).order('created_at DESC')
+    @items = Item.where(status: 0).page(params[:page]).per(6).order('created_at DESC')
     # アイテム表示件数
     @item_count = Item.all
   end
